9b484620368bf0cd20234cefc065ac28c1dfb55d
matsuo_takuro@retail-ai.jp <matsuo_takuro@retail-ai.jp>
2023-01-23 00:29:37 +0900

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE3OTQyMg/spectral.js?branch_slug=main&token=932b9929-913f-4d78-88ee-508210862383";
+export default { extends: ruleset };
