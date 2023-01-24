d671dca1883b8dd56f82ee60e444739ac91b0dca
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2023-01-25 08:22:33 +0900

    add health check endpoint for verification of normal operation

--- router/router.go
+++ router/router.go
@@ -16,19 +16,20 @@ import (
 
 func NewRouter(ctx context.Context, cfg *config.Config) (http.Handler, func(), error) {
 
-	// DBに接続する。
+	// DBに接続する
 	db, cleanup, err := repository.OpenDB(ctx, cfg)
 	if err != nil {
 		return nil, cleanup, fmt.Errorf("failed to open db: %w", err)
 	}
 
-	// その他ルーティングのための各種準備をする。
+	// その他ルーティングのための各種準備をする
 	r := chi.NewRouter()
 	v := validator.New()
 	c := clock.RealClocker{}
 	repo := repository.Repository{Clocker: c}
 
-	// 以下、各API（ハンドラ）に対して、公開するパスを割り当てる。
+	// 以下、各API（ハンドラ）に対して、実装されたservice・dbと、公開するpathを割り当てる
+
 	ru := &api.RegisterUser{
 		Service: &service.RegisterUserServiceImpl{
 			DB:   db,
