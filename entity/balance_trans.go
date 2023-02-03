17d2d8e7e298af173041d0b3feda477404056867
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2023-02-03 20:40:10 +0900

    add todos for reconsideration

--- entity/balance_trans.go
+++ entity/balance_trans.go
@@ -6,8 +6,8 @@ type BalanceTransID uint64
 
 type BalanceTrans struct {
 	ID            BalanceTransID `db:"id"`
-	UserID        UserID         `db:"user_id"`
-	BalanceID     BalanceID      `db:"balance_id"`
+	UserID        UserID         `db:"user_id"`    // TODO: 不要？Balanceのstructだけで良かった？かも
+	BalanceID     BalanceID      `db:"balance_id"` // TODO: Balanceのstructのほうが良かった？かも
 	TransferTrans TransferTrans  `db:"transfers"`
 	Amount        int32          `db:"amount"`
 	ProcessedAt   time.Time      `db:"processed_at"`
--- entity/transfer_trans.go
+++ entity/transfer_trans.go
@@ -6,10 +6,10 @@ type TransferTransID uint64
 
 type TransferTrans struct {
 	ID          TransferTransID `db:"id"`
-	FromUser    UserID          `db:"from_user"`
-	FromBalance BalanceID       `db:"from_balance"`
-	ToUser      UserID          `db:"to_user"`
-	ToBalance   BalanceID       `db:"to_balance"`
+	FromUser    UserID          `db:"from_user"`    // TODO: 不要？Balanceのstructだけで良かったかも
+	FromBalance BalanceID       `db:"from_balance"` // TODO: Balanceのstructのほうが良かった？かも
+	ToUser      UserID          `db:"to_user"`      // TODO: 不要？Balanceのstructだけで良かったかも
+	ToBalance   BalanceID       `db:"to_balance"`   // TODO: Balanceのstructのほうが良かった？かも
 	Amount      uint32          `db:"amount"`
 	ProcessedAt time.Time       `db:"processed_at"`
 }
